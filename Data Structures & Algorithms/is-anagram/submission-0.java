23b941a487051257a3ed576acdc448c96fe7b7ee
Vamsi Krishna Achyutuni <achyutunivk@gmail.com>
2026-09-08 23:03:12 +0530

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+       char[] a = s.toCharArray();
+       Arrays.sort(a);
+       char[] b = t.toCharArray();
+       Arrays.sort(b);
+       if (Arrays.equals(a, b)){
+        return true;
+       }
+       return false;
+
+    }
+}
